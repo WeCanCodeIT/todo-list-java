fa0aefd4bdbef7fa3a556a2eea24cab78aee1256
Benjamin Williams <DeanPBR@gmail.com>
2020-10-08 12:11:52 -0400

    Thursday workshop

--- src/main/java/org/wcci/todolist/TodoItemController.java
+++ src/main/java/org/wcci/todolist/TodoItemController.java
@@ -11,7 +11,7 @@ public class TodoItemController {
 
     @RequestMapping("sample-todo-item")
     public String showOneTodoItem(Model model){
-        TodoOwner sampleTodoOwner = new TodoOwner("Bosco", Collections.EMPTY_LIST);
+        TodoOwner sampleTodoOwner = new TodoOwner("Bosco", Collections.EMPTY_LIST, 1L);
         TodoItem sampleTodoItem = new TodoItem(sampleTodoOwner, "Go outside and bark", "Urgent", "Self", "Go outside to bark at squirrels");
 
         model.addAttribute("todoItem", sampleTodoItem);
--- src/main/java/org/wcci/todolist/TodoOwner.java
+++ src/main/java/org/wcci/todolist/TodoOwner.java
@@ -3,14 +3,19 @@ package org.wcci.todolist;
 import java.util.Collection;
 
 public class TodoOwner {
+    private long id;
     private String name;
     private Collection<TodoItem> items;
 
-    public TodoOwner(String name, Collection<TodoItem> items) {
+    public TodoOwner(String name, Collection<TodoItem> items, long id) {
+        this.id = id;
         this.name = name;
         this.items = items;
     }
 
+    public long getId(){
+        return id;
+    }
     public String getName() {
         return name;
     }
